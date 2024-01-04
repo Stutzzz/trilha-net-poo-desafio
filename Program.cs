0cd550e7570d9d279ad8c5c75e657a2e35ed349e
Stutzzz <leandrostutzsilva14@gmail.com>
2024-01-04 17:39:24 -0300

    Desafio do celular com POO no Bootcamp da DIO(Decola Tech 2024) Avanade

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Apple");
+Iphone iphone = new Iphone("1234","Iphone 14 Pro Max", "BHS825D", 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Youtube");
+
+Console.WriteLine("");
+
+Console.WriteLine("Smartphone Nokia");
+Nokia nokia = new Nokia("56789","Modelo 1", "KHDNF4572", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
